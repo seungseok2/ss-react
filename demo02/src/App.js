d35a7a5c00348948776d93a64d371bee222503d7
seungseok2 <1998_225@naver.com>
2023-10-30 14:18:42 +0900

    생성

---
+++ demo02/src/App.js
@@ -0,0 +1,19 @@
+import nsflogo from './nsflogo.jpg';
+
+//JSX에서는 프로그래밍 변수를 속성에 넣기 위해 {}를 사용한다
+//- src="hello"로 되어 있으면 경로를 의미
+//- src={hello}로 되어있으면 변수를 의미
+
+//JSX에서는 모든 태그가 닫혀야 한다
+function App() {
+var width=300;
+
+  return (
+    <>
+      <h2>KH정보교육원 리액트 수업자료</h2>
+      <img src={nsflogo} width={width} />
+    </>    
+  );
+}
+
+export default App;
